04eca8290d1692354ad6b9b2efc706ddbed7e12a
wilkye <wilkylegreer@gmail.com>
2026-03-20 09:52:44 -0600

    Added roll command logic

--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -3,9 +3,23 @@ import { SlashCommandBuilder } from "discord.js";
 const cmd = {
     data: new SlashCommandBuilder()
         .setName("roll")
-        .setDescription("Rolls a dice of your choice."),
+        .setDescription("Rolls a dice of your choice.")
+        .addStringOption(option =>
+            option.setName("choice")
+                .setDescription("Choose a Dice")
+                .setRequired(true)
+                .setChoices(
+                    { name: 'd4', value: '4' },
+                    { name: 'd6', value: '6' },
+                    { name: 'd10', value: '10' },
+                    { name: 'd20', value: '20' }
+                )
+        ),
     async execute(interaction) {
-        await interaction.reply("Ping!");
+        const userChoice = interaction.options.getString("choice");
+
+        const randomNum = Math.floor(Math.random() * parseInt(userChoice)) + 1
+        await interaction.reply(`You rolled a ${randomNum}`);
     }
 }
 
